a5ae92df343f4b9b414ae86caa35d093c907d241
GemButcher <gembutcher93@gmail.com>
2026-07-05 19:53:01 +0200

    Add files via upload
    fix 1.0

--- js/BootScene.js
+++ js/BootScene.js
@@ -2,22 +2,57 @@ class BootScene extends Phaser.Scene {
   constructor() { super('Boot'); }
 
   preload() {
-    // Carica da base64 invece che da file — niente CORS, funziona ovunque
-    this.load.spritesheet('sprites', ASSETS.sprite_png, {
-      frameWidth: SPRITE.FRAME_W,
-      frameHeight: SPRITE.FRAME_H
-    });
-    this.load.spritesheet('bot', ASSETS.bot_sprite_png, {
-      frameWidth: SPRITE.FRAME_W,
-      frameHeight: SPRITE.FRAME_H
-    });
-    this.load.image('mappa', ASSETS.mappa_png);
+    // Nessun caricamento esterno — tutto viene fatto in create()
   }
 
   create() {
-    this.makeProceduralTextures();
-    this.createAnimations();
-    this.scene.start('Menu');
+    this.loadCount = 0;
+    this.totalToLoad = 3;
+    this.onAllLoaded = () => {
+      this.makeProceduralTextures();
+      this.createAnimations();
+      this.scene.start('Menu');
+    };
+    this.loadBase64Textures();
+  }
+
+  loadBase64Textures() {
+    // Carica sprite sheet giocatori
+    const imgSprites = new Image();
+    imgSprites.onload = () => {
+      this.textures.addSpriteSheet('sprites', imgSprites, {
+        frameWidth: SPRITE.FRAME_W,
+        frameHeight: SPRITE.FRAME_H
+      });
+      this.checkLoaded();
+    };
+    imgSprites.src = ASSETS.sprite;
+
+    // Carica sprite sheet bot
+    const imgBot = new Image();
+    imgBot.onload = () => {
+      this.textures.addSpriteSheet('bot', imgBot, {
+        frameWidth: SPRITE.FRAME_W,
+        frameHeight: SPRITE.FRAME_H
+      });
+      this.checkLoaded();
+    };
+    imgBot.src = ASSETS.bot;
+
+    // Carica mappa
+    const imgMappa = new Image();
+    imgMappa.onload = () => {
+      this.textures.addImage('mappa', imgMappa);
+      this.checkLoaded();
+    };
+    imgMappa.src = ASSETS.mappa;
+  }
+
+  checkLoaded() {
+    this.loadCount++;
+    if (this.loadCount >= this.totalToLoad) {
+      this.onAllLoaded();
+    }
   }
 
   makeProceduralTextures() {
@@ -59,85 +94,33 @@ class BootScene extends Phaser.Scene {
 
     // --- Animazioni giocatori (spritesheet 'sprites') ---
     genders.forEach(gender => {
-      const off = gender === 'FEMALE' ? 22 : 0;
       const pre = gender === 'FEMALE' ? 'f_' : 'm_';
       const map = SPRITE[gender];
 
       dirs.forEach(d => {
-        this.anims.create({
-          key: pre + 'idle_' + d,
-          frames: [{ key: 'sprites', frame: map['idle_' + d] }],
-          frameRate: 1, repeat: -1
-        });
+        this.anims.create({ key: pre + 'idle_' + d, frames: [{ key: 'sprites', frame: map['idle_' + d] }], frameRate: 1, repeat: -1 });
       });
-
-      this.anims.create({
-        key: pre + 'walk_front',
-        frames: [{ key: 'sprites', frame: map.idle_front }, { key: 'sprites', frame: map.walk_front }],
-        frameRate: 4, repeat: -1
-      });
-      this.anims.create({
-        key: pre + 'walk_side',
-        frames: [{ key: 'sprites', frame: map.idle_side }, { key: 'sprites', frame: map.walk_side }],
-        frameRate: 4, repeat: -1
-      });
-      this.anims.create({
-        key: pre + 'walk_back',
-        frames: [{ key: 'sprites', frame: map.idle_back }, { key: 'sprites', frame: map.walk_back }],
-        frameRate: 4, repeat: -1
-      });
-
+      this.anims.create({ key: pre + 'walk_front', frames: [{ key: 'sprites', frame: map.idle_front }, { key: 'sprites', frame: map.walk_front }], frameRate: 4, repeat: -1 });
+      this.anims.create({ key: pre + 'walk_side',  frames: [{ key: 'sprites', frame: map.idle_side },  { key: 'sprites', frame: map.walk_side }],  frameRate: 4, repeat: -1 });
+      this.anims.create({ key: pre + 'walk_back',  frames: [{ key: 'sprites', frame: map.idle_back },  { key: 'sprites', frame: map.walk_back }],  frameRate: 4, repeat: -1 });
       dirs.forEach(d => {
-        this.anims.create({
-          key: pre + 'shoot_' + d,
-          frames: [{ key: 'sprites', frame: map['shoot_' + d] }],
-          frameRate: 1, repeat: 0
-        });
+        this.anims.create({ key: pre + 'shoot_' + d, frames: [{ key: 'sprites', frame: map['shoot_' + d] }], frameRate: 1, repeat: 0 });
       });
-
-      this.anims.create({
-        key: pre + 'death',
-        frames: this.anims.generateFrameNumbers('sprites', { start: map.death_start, end: map.death_end }),
-        frameRate: 8, repeat: 0
-      });
+      this.anims.create({ key: pre + 'death', frames: this.anims.generateFrameNumbers('sprites', { start: map.death_start, end: map.death_end }), frameRate: 8, repeat: 0 });
     });
 
     // --- Animazioni bot (spritesheet 'bot') ---
     const botMap = SPRITE.BOT;
     const bPre = 'bot_';
     dirs.forEach(d => {
-      this.anims.create({
-        key: bPre + 'idle_' + d,
-        frames: [{ key: 'bot', frame: botMap['idle_' + d] }],
-        frameRate: 1, repeat: -1
-      });
+      this.anims.create({ key: bPre + 'idle_' + d, frames: [{ key: 'bot', frame: botMap['idle_' + d] }], frameRate: 1, repeat: -1 });
     });
-    this.anims.create({
-      key: bPre + 'walk_front',
-      frames: [{ key: 'bot', frame: botMap.idle_front }, { key: 'bot', frame: botMap.walk_front }],
-      frameRate: 4, repeat: -1
-    });
-    this.anims.create({
-      key: bPre + 'walk_side',
-      frames: [{ key: 'bot', frame: botMap.idle_side }, { key: 'bot', frame: botMap.walk_side }],
-      frameRate: 4, repeat: -1
-    });
-    this.anims.create({
-      key: bPre + 'walk_back',
-      frames: [{ key: 'bot', frame: botMap.idle_back }, { key: 'bot', frame: botMap.walk_back }],
-      frameRate: 4, repeat: -1
-    });
+    this.anims.create({ key: bPre + 'walk_front', frames: [{ key: 'bot', frame: botMap.idle_front }, { key: 'bot', frame: botMap.walk_front }], frameRate: 4, repeat: -1 });
+    this.anims.create({ key: bPre + 'walk_side',  frames: [{ key: 'bot', frame: botMap.idle_side },  { key: 'bot', frame: botMap.walk_side }],  frameRate: 4, repeat: -1 });
+    this.anims.create({ key: bPre + 'walk_back',  frames: [{ key: 'bot', frame: botMap.idle_back },  { key: 'bot', frame: botMap.walk_back }],  frameRate: 4, repeat: -1 });
     dirs.forEach(d => {
-      this.anims.create({
-        key: bPre + 'shoot_' + d,
-        frames: [{ key: 'bot', frame: botMap['shoot_' + d] }],
-        frameRate: 1, repeat: 0
-      });
+      this.anims.create({ key: bPre + 'shoot_' + d, frames: [{ key: 'bot', frame: botMap['shoot_' + d] }], frameRate: 1, repeat: 0 });
     });
-    this.anims.create({
-      key: bPre + 'death',
-      frames: this.anims.generateFrameNumbers('bot', { start: botMap.death_start, end: botMap.death_end }),
-      frameRate: 8, repeat: 0
-    });
+    this.anims.create({ key: bPre + 'death', frames: this.anims.generateFrameNumbers('bot', { start: botMap.death_start, end: botMap.death_end }), frameRate: 8, repeat: 0 });
   }
 }
