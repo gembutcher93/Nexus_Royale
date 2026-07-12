3ecdccbaa6f599ee840810e76a32888ae7c3f7aa
GemButcher <gembutcher93@gmail.com>
2026-07-12 22:36:16 +0200

    fix update game

--- sw.js
+++ sw.js
@@ -7,4 +7,4 @@ const ASSETS=['./index.html','./game.js','./assets_fallback.js','./manifest.json
   'https://cdnjs.cloudflare.com/ajax/libs/phaser/3.80.1/phaser.min.js'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})); self.skipWaiting(); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))); self.clients.claim(); });
-self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+self.addEventListener('fetch',e=>{ e.respondWith(fetch(e.request).then(res=>{ const cp=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return res; }).catch(()=>caches.match(e.request))); });
