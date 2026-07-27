408aa3c99319b6c2d2e43c942c301a062f9f1e9c
GemButcher <gembutcher93@gmail.com>
2026-07-27 14:37:33 +0200

    Update tint color to white in game.js
    Change tint color from dynamic to white.

--- game.js
+++ game.js
@@ -2244,7 +2244,7 @@ class Game extends Phaser.Scene{
           this.add.tileSprite(bx,by,bw,bh,'sw_f').setOrigin(0,0).setDepth(-18).setTileScale(0.5,0.5);
         continue;
       }
-      const tint=this.ndCol? this.ndCol(bx+bw/2,by+bh/2) : 0xff3355;
+      cconst tint=0xffffff;
       // 1) RIEMPIMENTO: lastricato bordeaux dentro tutta la piazzetta (niente piu' buco nero)
       if(this.textures.exists('sw_f'))
         this.add.tileSprite(bx,by,bw,bh,'sw_f').setOrigin(0,0).setDepth(-18.5).setTileScale(0.5,0.5).setTint(tint);
